cb22a69c8edfbb7daa6b30f9e70d1480ce61ccbd
Shimipon <4mizu.tt@gmail.com>
2019-05-08 21:41:08 +0900

    BFS

--- Antbook_syokyu/例題2-1-3幅優先探索/ABC088D.cpp
+++ Antbook_syokyu/例題2-1-3幅優先探索/ABC088D.cpp
@@ -3,44 +3,57 @@
 #include <vector>
 #include <cmath>
 #include <map>
+#include <utility>
+#include <queue>
 using namespace std;
-bool used [8];
-int perm[8];
-int a[28], b[28];
+typedef pair<int, int> Pos;
+vector<int> dx = {1, -1, 0, 0};
+vector<int> dy = {0, 0, 1, -1};
+int INF = 99999999;
 
-int permutation(int pos, int n, int m){
-	if(pos == n){
-		bool check = true;
-		for(int i=0;i<n-1;i++){
-			bool check2 = false;//辺がないときにfalse
-			for(int j=0;j < m;j++){
-				if(a[j]-1 == perm[i] && b[j]-1 == perm[i+1]) check2 = true;
-				if(b[j]-1 == perm[i] && a[j]-1 == perm[i+1]) check2 = true;
-			}
-			check = check2;
-			if(!check) break;
-		}
-		if(check) return 1;
-		else return 0;
-	}
-	int ans = 0;
-	for(int i=0; i<n;i++){
-		if(!used[i]){
-			perm[pos] = i;
-			used[i] = true;
-			ans += permutation(pos + 1, n, m);
-			used[i] = false;
-		}
-	}
-	return ans;
+bool check(Pos p, int r, int c){
+	if (p.first < 0) return false;
+	if (p.first >= r) return false;
+	if (p.second < 0) return false;
+	if (p.second >= c) return false;
+	return true;
 }
+	
+
 int main(){
-	int N, M;
-	cin >> N >> M;
-	for(int i=0;i<M;i++){
-		cin >> a[i] >> b[i];
+	int R, C;
+	cin >> R >> C;
+	Pos S, G;
+	cin >> S.first >> S.second >> G.first >> G.second;
+	S.first--;
+	S.second--;
+	G.first--;
+	G.second--;
+	vector<string> c(R);
+	for(int i=0;i<R;i++){
+		cin >> c[i];
 	}
-	used[0] = true;
-	perm[0] = 0;
-	cout << permutation(1,N,M) << endl;
+	int Reach[R][C];
+	for(int i=0;i<R;i++){
+		for(int j=0;j<C;j++){
+			Reach[i][j] = INF;
+		}
+	}
+	queue<Pos> Q;
+	Q.push(S);
+	Reach[S.first][S.second] = 0;
+	while(!Q.empty()){
+		Pos now = Q.front();
+		Q.pop();
+		for(int i=0;i<4;i++){
+			Pos next;
+			next.first = now.first + dx[i];
+			next.second = now.second + dy[i];
+			if(check(next, R, C) && c[next.first][next.second] == '.' && Reach[next.first][next.second] == INF){
+				Reach[next.first][next.second] = Reach[now.first][now.second] + 1;
+				Q.push(next);
+			}
+		}
+	}
+	cout << Reach[G.first][G.second] << endl;
 }
